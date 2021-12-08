9d87cff115f959d5925b717fb40c96cecda5e3cd
SpencerTower <spencerjtower1@gmail.com>
2021-12-07 19:16:21 -0800

    updates API endpoint

--- server.js
+++ server.js
@@ -5,16 +5,19 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config(); // lets you use env file
+const weatherData = require('./data/weather.json');
 
 const app = express(); // gives back return value
 console.log(app);
 
 app.use(cors()); // acts as middleware - intermediary between server and requests
 
-app.get('/test', handleGetRequest);
+app.get('/weather', handleGetWeather);
 
-function handleGetRequest(request, response) {
-  response.send
+function handleGetWeather(request, response) {
+  // sends weather array and 200 (ok) response
+  response.status(200).send(weatherData);
+  // with status
 }
 
 
