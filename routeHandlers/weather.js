24a51f6fc034aa65221b7a5d3b3a7d1c956c0dd5
SpencerTower <spencerjtower1@gmail.com>
2021-12-12 12:40:53 -0800

    adds weatherReqHandler() to server.js, adds parseWeatherData() to weather.js

--- routeHandlers/weather.js
+++ routeHandlers/weather.js
@@ -2,21 +2,31 @@
 
 const axios = require('axios');
 
+
+// refactor to check query and recieve lat, lon
 async function handleGetWeather(req, res) {
 
-  console.log(req.query);
+  const weatherbitUrl = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=7`;
 
   try {
-    const liveWeather = await axios.get(`http://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=7`);
+    const
+    const liveWeather = await axios.get(weatherbitUrl);
     console.log(liveWeather.data);
     const liveWeatherRes = liveWeather.data.data.map(day => new Forecast(day));
     // send city weather data description to client
     res.status(200).send(liveWeatherRes);
   } catch (error) {
 
-    res.status(400).send(`There was an error retrieving weather data for ${req.query.city_name}.`);
+    res.status(500).send(`There was an error retrieving weather data for ${req.query.city_name}.`);
   }
 }
+
+// parse weather data with Forecast object constructor
+function parseWeatherData(weatherData) {
+  const forecast = weatherData.data.data.map(day => new Forecast(day));
+  return forecast;
+}
+
 
 class Forecast {
   constructor(obj) {
--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express(); // gives back return value
 app.use(cors()); // acts as middleware - intermediary between server and requests
-const handleGetWeather = require('./routeHandlers/weather');
+// import getWeatherData() from weather.js
+const getWeatherData = require('./routeHandlers/weather');
 const handleGetMovie = require('./routeHandlers/movies');
 
 // open route for weather
-app.get('/weather', handleGetWeather);
+app.get('/weather', weatherReqHandler);
 // open route for movies
 app.get('/movies', handleGetMovie);
 // send error back to client if page not found
@@ -19,4 +20,13 @@ app.get('/*', (req, res) => res.status(404).send('Route Not Found'));
 // turn on server
 app.listen(process.env.PORT, () => console.log('server is listening on PORT 3001'));
 
+// handle req from client, pass to weather.js, send res to client
+function weatherReqHandler(req, res) {
+  // assign lat lon from query to variables
+  const { lat, lon } = req.query;
+  // pass lat and lon into getWeather() in weather.js to get data from cache or new axios request
+  const weatherData = getWeatherData(lat, lon);
+  res.status(200).send(weatherData);
+  // add error handler
+}
 
